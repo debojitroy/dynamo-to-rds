58deac75999bda1b371231d92dc38d56ae93248c
debojitr <debojitr@amazon.com>
2023-05-01 15:46:44 +0530

    Added DynamoDB Stream consumer lambda function

--- deployments/infra/main.tf
+++ deployments/infra/main.tf
@@ -48,7 +48,7 @@ resource "aws_dynamodb_table" "pg_router_table" {
   }
 
   stream_enabled = true
-  stream_view_type = "NEW_IMAGE"
+  stream_view_type = "NEW_AND_OLD_IMAGES"
 
   tags = {
     Type = "DynamoDB Table"
@@ -57,5 +57,132 @@ resource "aws_dynamodb_table" "pg_router_table" {
   }
 }
 
+##
+# Start - Preparing the Lambda Function
+##
+
+# Base Policy for Lambda
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# Create Lambda Execution Role
+resource "aws_iam_role" "dynamodb_consumer_lambda_role" {
+  name               = "dynamodb_consumer_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+# Create Cloudwatch Group for Lambda
+resource "aws_cloudwatch_log_group" "pg_router_dynamodb_consumer_cw_log_group" {
+  name              = "/aws/lambda/pg_router_dynamodb_consumer"
+  retention_in_days = 3
+}
+
+## Create Additional Policy for logging
+data "aws_iam_policy_document" "dynamodb_consumer_lambda_logging_statement" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "dynamodb_consumer_lambda_logging_policy" {
+  name        = "dynamodb_consumer_lambda_logging_policy"
+  path        = "/"
+  description = "IAM policy for logging from a PG Router DynamoDB consumer lambda"
+  policy      = data.aws_iam_policy_document.dynamodb_consumer_lambda_logging_statement.json
+}
+
+# Attach to Lambda Policy - CW Logging
+resource "aws_iam_role_policy_attachment" "dynamo_consumer_lambda_logs" {
+  role       = aws_iam_role.dynamodb_consumer_lambda_role.name
+  policy_arn = aws_iam_policy.dynamodb_consumer_lambda_logging_policy.arn
+}
+
+# Create additional policy to access Dynamodb Streams
+data "aws_iam_policy_document" "dynamodb_consumer_lambda_access_dynamo_streams_statement" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:DescribeStream",
+      "dynamodb:ListStreams"
+    ]
+
+    resources = ["arn:aws:dynamodb:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "dynamodb_consumer_lambda_stream_policy" {
+  name        = "dynamodb_consumer_lambda_stream_policy"
+  path        = "/"
+  description = "IAM policy for accessing dynamodb stream from PG Router DynamoDB consumer lambda"
+  policy      = data.aws_iam_policy_document.dynamodb_consumer_lambda_access_dynamo_streams_statement.json
+}
+
+# Attach to Lambda Policy - Stream Access
+resource "aws_iam_role_policy_attachment" "dynamo_consumer_lambda_stream" {
+  role       = aws_iam_role.dynamodb_consumer_lambda_role.name
+  policy_arn = aws_iam_policy.dynamodb_consumer_lambda_stream_policy.arn
+}
 
 
+# Prepare the Archive
+data "archive_file" "dynamodb_consumer_lambda_zip" {
+  type        = "zip"
+  source_file = "../${path.module}/dist/consumer/main"
+  output_path = "../${path.module}/dist/consumer/dynamodb_consumer_lambda_payload.zip"
+}
+
+# Create the lambda function
+resource "aws_lambda_function" "dynamodb_consumer_lambda" {
+  filename      = "../${path.module}/dist/consumer/dynamodb_consumer_lambda_payload.zip"
+  function_name = "pg_router_dynamodb_consumer"
+  role          = aws_iam_role.dynamodb_consumer_lambda_role.arn
+  handler       = "main"
+
+  source_code_hash = data.archive_file.dynamodb_consumer_lambda_zip.output_base64sha256
+
+  runtime = "go1.x"
+
+  environment {
+    variables = {
+      env = var.env
+    }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.dynamo_consumer_lambda_logs,
+    aws_iam_role_policy_attachment.dynamo_consumer_lambda_stream,
+    aws_cloudwatch_log_group.pg_router_dynamodb_consumer_cw_log_group,
+  ]
+}
+
+##
+# End - Preparing the Lambda Function
+##
+
+# Create DynamoDB Stream Event Source mapping for Lambda
+resource "aws_lambda_event_source_mapping" "pg_router_dynamodb_stream_event_mapping" {
+  event_source_arn  = aws_dynamodb_table.pg_router_table.stream_arn
+  function_name     = aws_lambda_function.dynamodb_consumer_lambda.arn
+  starting_position = "LATEST"
+}
